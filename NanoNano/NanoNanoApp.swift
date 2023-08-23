b5e577d902d97d985bcf7381ee8edd763408adde
Muhammad Rezky <mrezkysulihin@gmail.com>
2023-08-23 09:29:29 +0700

    Initial Commit

---
+++ NanoNano/NanoNanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoNanoApp.swift
+//  NanoNano
+//
+//  Created by Muhammad Rezky on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoNanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
